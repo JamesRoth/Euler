e970b52d0c113012224bd635bd151d214268e4b8
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-07 13:13:22 -0400

    Updated from Brython Server: 5/7/2018 1:13:21 PM

--- multipules3and5.py
+++ multipules3and5.py
@@ -2,12 +2,13 @@
 #5/7/18
 #mulipules3and5.py
 
-total = 0
+total = []
 
 for i in range(3,1000,3):
     total+=i
 
 for i in range(5,1000,5):
-    total+=i
+    if i%3!=0:
+        total+=i
 
 print(total)
