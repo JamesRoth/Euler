0f49bce9a233556a5f77eb782db1044087cb21d9
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-07 13:20:13 -0400

    Updated from Brython Server: 5/7/2018 1:20:13 PM

--- evenFibonacci.py
+++ evenFibonacci.py
@@ -2,4 +2,13 @@
 #5/7/18
 #evenFibonacci.py - sum of even fibonacci numbers under 4 million
 
-while 
+num=0
+
+fib=[1,2]
+
+while num<4000000:
+    num=fib[-1]+fib[-2]
+    fib.append(num)
+    
+for item in fib:
+    if item
