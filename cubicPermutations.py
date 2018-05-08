03a92020fe27938cccbe15b640bc9b064fc4e6a2
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-08 13:43:58 -0400

    Updated from Brython Server: 5/8/2018 1:43:58 PM

--- cubicPermutations.py
+++ cubicPermutations.py
@@ -8,7 +8,7 @@ if abs(round(1000**(1/3),0) - (1000**(1/3))) < 0.0000000001:
 
 print(1000**(1/3))
 """
-
+"""
 from itertools import permutations
 
 def consolidate(permute): #the permuated numbers are lists within a list, this just makes them into numbers in a list
@@ -36,8 +36,7 @@ while cubeMe==5027:
         print(cubeMe)
         done=1
     else:
-        print(fiveCubes)
-        print(cubeMe)
         cubeMe+=1
-
-
+"""
+for p in permutations(str(127035954683)):
+    print(p)
