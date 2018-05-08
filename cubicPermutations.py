9a927ad5bbbb843c66488212b64a1143af7a40fa
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-08 08:03:35 -0400

    Update cubicPermutations.py

--- cubicPermutations.py
+++ cubicPermutations.py
@@ -2,6 +2,10 @@
 #5/7/18
 #cubicPermuations.py - smallest cube that can be permuted into 5 different cubes
 
+from itertools import permutations
+
+
+"""
 cubeMe=1
 done=0
 origPermute=[]
@@ -10,3 +14,8 @@ while done==0:
     cube=cubeMe**3
     for num in cube:
         origPermute.append
+"""
+
+list1 = ["a", "b", c", "d"]
+for p in permutations(list1):
+    print(p)
