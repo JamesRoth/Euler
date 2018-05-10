66ec1bf1435fcce3ce421425ffc8234b293d916d
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-10 11:44:04 -0400

    Updated from Brython Server: 5/10/2018 11:44:04 AM

--- e3.py
+++ e3.py
@@ -7,10 +7,10 @@ from math import *
 def loopMe(num):
     for i in range(floor(num**0.5),2,-1):
         if num%i==0:
-            data["num1"]=num/i
+            data["num1"]=data["num1"]/i
             data["factors"].append(i)
-            print(num)
+            print(data["num1"])
             print(data["factors"])
             loopMe(num)
 
-loopMe(data"[num1"])
+loopMe(data["num1"])
