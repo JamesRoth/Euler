80ed0077738b0b25c9cbf1bceba90091dc4d3433
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-10 11:48:55 -0400

    Updated from Brython Server: 5/10/2018 11:48:54 AM

--- e3.py
+++ e3.py
@@ -23,7 +23,7 @@ factors=[]
 from math import *
 
 for i in range(floor(num**0.5),1,-1):
-    if num%i==0:
+    if num%i==0 and i!=num:
         num=num/i
         factors.append(i)
         print(factors)
