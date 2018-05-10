8364f68bba680fe66ebbe7c8d98b4c5631b57b01
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-10 11:26:22 -0400

    Updated from Brython Server: 5/10/2018 11:26:21 AM

--- e3.py
+++ e3.py
@@ -5,4 +5,9 @@ loopMe(num):
     for i in range(floor(num),1,-1):
         if num%i==0:
             num1=num/i
+            factors.append(i)
+            print(num)
+            print(factors)
             loopMe(num)
+
+loopMe(num1)
