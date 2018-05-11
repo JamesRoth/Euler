c142203dc6059c0b638b6cd1a0fdd18e4bccd6b8
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-11 15:13:39 -0400

    Updated from Brython Server: 5/11/2018 3:13:38 PM

--- cubicPermutations.py
+++ cubicPermutations.py
@@ -30,9 +30,11 @@ while cubeMe<=6000: #main loop
     cubes.append(sortInt(cubeMe**3)) #inputs cubes, then sorts them, returns a list of digits
     if cubeMe == 6000:
         ans = 0
-        ans = checkList(cubes)
+        #ans = checkList(cubes)
         if ans != "none": #if match returned, print match
             print("Ans: ", ans)
         if ans == "none":
             print("Failed")
     cubeMe+=1
+    print(cubeMe)
+    print(cubeMe**3)
