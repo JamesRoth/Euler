8aa6ad5243fb990a102400002a9ce92462c09938
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-11 15:05:48 -0400

    Updated from Brython Server: 5/11/2018 3:05:47 PM

--- cubicPermutations.py
+++ cubicPermutations.py
@@ -13,12 +13,15 @@ def sortInt(num): #sorts an individual list item
     return(listSort)
 
 def checkList(cubes):
+    """
     for i in range(len(cubes)-1, 0,-1): #checks sorted list for matches
         if cubes.count(cubes[i]) == 3: #if exactly five matches, return cube that matched
             print(cubes[i])
             return(i)
     print(cubes)
     return("none")
+    """
+    print(cubes.count(cubes[5027]))
 
 while cubeMe<=6000: #main loop
     cubes.append(sortInt(cubeMe**3)) #inputs cubes, then sorts them, returns a list of digits
