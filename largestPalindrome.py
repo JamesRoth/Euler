1fbc81bb0965906f3cd3c807bc7ec2d8c378d5de
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-14 12:12:34 -0400

    Updated from Brython Server: 5/14/2018 12:12:32 PM

--- largestPalindrome.py
+++ largestPalindrome.py
@@ -7,6 +7,7 @@ def palindrome(num):
     for i in range(0, len(str(num))):
         list1.append(int(str(num)[i]))
     list2 = list1.reverse
+    print(list1)
     if list1 == list2:
         print("yes")
     else:
