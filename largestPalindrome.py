c0b939f0ddef7d69587e6757b8d00982b0d22199
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-14 12:05:43 -0400

    Updated from Brython Server: 5/14/2018 12:05:40 PM

--- largestPalindrome.py
+++ largestPalindrome.py
@@ -2,4 +2,11 @@
 #5/14/18
 #largestPalindrome.py - largest palindrome from product of two three digit numbers
 
-
+def palindrome(num):
+    list1 = []
+    for item in str(num):
+        list1.append(int(item))
+    if list1 == list1.reverse:
+        print("yes")
+    else:
+        print("no")
