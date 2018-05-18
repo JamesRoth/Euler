05fb9dd04c51c2ba161511934e45f88fdbbe6f05
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-18 15:06:08 -0400

    Updated from Brython Server: 5/18/2018 3:06:08 PM

--- divisible1-20.py
+++ divisible1-20.py
@@ -2,7 +2,7 @@
 #5/17/18
 #divisible1-20.py - smallest number that is divisible by 1-20 evenly
 
-num = 0
+num = 2432902008176640000
 
 factors = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
