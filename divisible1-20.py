d02434ac1db48639d043bb742280cccf2f36e384
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-18 14:55:03 -0400

    Updated from Brython Server: 5/18/2018 2:55:02 PM

--- divisible1-20.py
+++ divisible1-20.py
@@ -2,7 +2,7 @@
 #5/17/18
 #divisible1-20.py - smallest number that is divisible by 1-20 evenly
 
-num = 380
+num = 0
 
 factors = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
@@ -14,4 +14,4 @@ while True:
     if check == 19:
         print(num)
         break
-    num+=2
+    num+=20
