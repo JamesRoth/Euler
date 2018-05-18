00709eb5d2b512ef30c2a251d16b906e97949a32
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-18 14:54:51 -0400

    Updated from Brython Server: 5/18/2018 2:54:51 PM

--- divisible1-20.py
+++ divisible1-20.py
@@ -4,14 +4,14 @@
 
 num = 380
 
-factors = [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+factors = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
 while True:
     check = 0
     for item in factors:
         if num%item == 0:
             check+=1
-    if check == 18:
+    if check == 19:
         print(num)
         break
     num+=2
