a4d31be369af8d1ec38870c8cdcf3b36f4ccc9da
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-22 12:07:29 -0400

    Updated from Brython Server: 5/22/2018 12:07:28 PM

--- 10001stPrime.py
+++ 10001stPrime.py
@@ -2,6 +2,14 @@
 #5/22/18
 #10001stPrime.py - finding the 10001st prime number
 
-from sieveOfE.py import sieve
+def sieve(num):
+    a = []
+    for i in range(2, num+1):
+        a.append(i)
+
+    for i in range(2, len(a)+1):
+        for j in range(i*2-2, num-1, i):
+            a[j] = 0
+    return(a)
 
 print(sieve(30))
