99f127216e860732ea7b0a77787c5f369c28ce86
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-06-06 10:46:32 -0400

    Updated from Brython Server: 6/6/2018 10:46:32 AM

--- 13adjacentDigits.py
+++ 13adjacentDigits.py
@@ -33,6 +33,6 @@ def product(list1):
 
 for i in range(1, len(str(num))):
     products.append(product(numbers))
-    list1=int(str(list1)[1:])
+    numbers=int(str(numbers)[1:])
     numbers.append(num[entry-1])
 print(max(products))
